75cb5a26c647daf5b5fba3f354dc251f41f63a11
dela-ger <gdjameh@yahoo.com>
2022-09-28 07:39:58 +0000

    Add some components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link, NavLink } from "react-router-dom";
 import "../css/style.css";
+import bars from "../Assets/bars-solid.svg";
+import xmark from "../Assets/xmark-solid.svg";
 
 function NavBar() {
   return (
     <nav>
-        <h2><NavLink>Kob's Blog</NavLink></h2>
-        <ul>
+        
+        <h2 ><NavLink className='nav-logo'>Kob's Blog</NavLink></h2>
+        <img src={bars} alt="humburger menu" className='menu-bar' />
+        <ul className='nav-list'>
             <li>
                 <NavLink to="/">Home</NavLink>
             </li>
