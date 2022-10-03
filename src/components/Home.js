3277f70e84cce98a1ac70eee50d1d1baee7d6f03
dela-ger <gdjameh@yahoo.com>
2022-10-03 11:32:43 +0000

    Added the blog generator function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,78 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import seven from "../Assets/17.svg";
+import arrowRight from "../Assets/arrow-right-solid.svg";
+import axios from 'axios';
+import Posts from "../data/data.json"
 
 function Home() {
+  const url = 'https://jsonplaceholder.typicode.com/todos/1'
+  const [posts, setPost] = useState([])
 
-  const [blog, setBlog] = useState([])
 
-  const getCard = () => {
-
+// function to put data into the posts state
+  const dataPost = () => {
+    setPost(Posts)
+    
   }
+  useEffect(() => {
+    dataPost()
+  },[])
+
+  console.log(posts)
   
+  function getPosts() {
+    let list = []
+    let result = []
+
+    posts.map(post => {
+      return list.push(
+        <div className="blog-card">
+          <div className="image-featured">
+            
+          </div>
+          <div className="title-block">
+            <div className="title">
+              <h3>{/*{Posts.map(post => {
+              return post
+            })}*/}{post.title}</h3>
+            </div>
+            <div className="date-btn">
+              <span id="date">
+                September,2022
+              </span>
+              <Link to="/blog">
+                <button className='more-btn'><img id='arrow-right' src={arrowRight} alt="arrow right" /></button>
+              </Link>
+            </div>
+          </div>
+          
+        </div>
+      )
+    })
+
+    // for(let i=0; i<list.length; i+=2){
+    //   result.push(
+    //     <div>
+    //       <div>
+    //         {list[i]}
+    //       </div>
+    //       <div>
+    //         {list[i+1] ? list[i+1] : null }
+    //       </div>
+    //     </div>
+    //   )
+    // }
+
+    for(let i=0; i<list.length; i++){
+      result.push(
+        list[i]
+      )
+    }
+
+    return result
+    
+  }
 
   return (
     <>
@@ -22,66 +85,8 @@ function Home() {
 
 <hr />
 
-      <section className="featured-blogs">
-        <div className="blog-card">
-          <div className="image-featured">
-            {/* <img id="title-svg" src={seven} alt="some svg" /> */}
-          </div>
-          <div className="title">
-            <h3>The Best Harry Potter Films</h3>
-          </div>
-          <div className="date-btn">
-            <span id="date">
-              September,2022
-            </span>
-            <button className='feat-btn'>More</button>
-          </div>
-        </div>
-
-        <div className="blog-card">
-          <div className="image-featured">
-            {/* <img id="title-svg" src={seven} alt="some svg" /> */}
-          </div>
-          <div className="title">
-            <h3>The Best Harry Potter Films</h3>
-          </div>
-          <div className="date-btn">
-            <span id="date">
-              September,2022
-            </span>
-            <button className='feat-btn'>More</button>
-          </div>
-        </div>
-
-        <div className="blog-card">
-          <div className="image-featured">
-            {/* <img id="title-svg" src={seven} alt="some svg" /> */}
-          </div>
-          <div className="title">
-          <h3>The Best Harry Potter Films</h3>
-          </div>
-          <div className="date-btn">
-            <span id="date">
-              September,2022
-            </span>
-            <button className='feat-btn'>More</button>
-          </div>
-        </div>
-
-       <div className="blog-card">
-          <div className="image-featured">
-            {/* <img id="title-svg" src={seven} alt="some svg" /> */}
-          </div>
-          <div className="title">
-          <h3>The Best Harry Potter Films</h3>
-          </div>
-          <div className="date-btn">
-            <span id="date">
-              September,2022
-            </span>
-            <button className='feat-btn'>More</button>
-          </div>
-        </div>
+      <section className="featured-blogs">        
+        {getPosts()}
       </section>
     </>
   )
